ece1366c1269028289a6d86ed7f1b217507ae807
Dawid Lech Socha <xDayfit@gmail.com>
2025-04-17 19:58:22 +0200

    update index.html to remove type attribute from script tag and configure Vite for dependency optimization

--- src/main/resources/static/vite.config.js
+++ src/main/resources/static/vite.config.js
@@ -1,5 +1,16 @@
 export default {
     define: {
         global: 'globalThis'
+    },
+    optimizeDeps: {
+        include: ['@stomp/stompjs']
+    },
+    build: {
+        rollupOptions: {
+            external: [],
+        },
+        commonjsOptions: {
+            include: [/node_modules/]
+        }
     }
 }
